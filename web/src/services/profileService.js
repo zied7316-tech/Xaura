d6c8f51ac46ba58817a5685705c0e2714acca4f9
zied7316-tech <zied7316-tech@users.noreply.github.com>
2025-11-24 17:47:32 +0100

    Fix profileService response structure and add regenerateUserID method

--- web/src/services/profileService.js
+++ web/src/services/profileService.js
@@ -4,13 +4,19 @@ export const profileService = {
   // Get current user profile
   getProfile: async () => {
     const response = await api.get('/profile')
-    return response.data.user
+    return response.data.data.user
   },
 
   // Update profile
   updateProfile: async (profileData) => {
     const response = await api.put('/profile', profileData)
-    return response.data.user
+    return response.data.data.user
+  },
+  
+  // Regenerate userID
+  regenerateUserID: async () => {
+    const response = await api.post('/profile/regenerate-userid')
+    return response.data.data.user
   }
 }
 
